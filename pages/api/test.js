6a225b0c6f7af0497c2bc7d13108a11dd51cf4da
thomasgit123 <thomasbox123@gmail.com>
2021-08-15 21:58:58 -0400

    feat: api routes

--- pages/api/test.js
+++ pages/api/test.js
@@ -3,7 +3,10 @@ const handlers = {
     return response.status(200).send();
   },
   GET: (request, response) => {
-    return response.status(200).send();
+    return response.send({
+      message: "This is test GET endpoint.",
+      hasuraURL: process.env.HASURA_BASE_URL,
+    });
   },
   POST: async (request, response) => {
     return response.send({
